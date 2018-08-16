bafbd690c89e6fefcae746399d268c37e89e8910
Matthew Peters <mattsterp@gmail.com>
2018-08-16 07:41:30 -0400

    added the edit button

--- app.js
+++ app.js
@@ -2,18 +2,33 @@ const form = document.getElementById('registrar');
 const input = form.querySelector('input');
 const ul = document.getElementById('invitedList');
 
-form.addEventListener('submit', (e) => {
-	e.preventDefault();
-	const text = input.value;
-	input.value = '';
+function createLI(text) {
 	const li = document.createElement('li');
 	li.textContent = text;
+
 	const label = document.createElement('label');
 	label.textContent = 'confirmed';
+
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 	label.appendChild(checkbox);
 	li.appendChild(label);
+
+	const editButton = document.createElement('button');
+	editButton.textContent = 'edit';
+	li.appendChild(editButton);
+
+	const removeButton = document.createElement('button');
+	removeButton.textContent = 'remove';
+	li.appendChild(removeButton);
+	return li;
+}
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	const text = input.value;
+	input.value = '';
+	const li = createLI(text);
 	ul.appendChild(li);
 });
 
@@ -26,5 +41,18 @@ ul.addEventListener('change', (e) => {
 		listItem.className = 'responded';
 	} else {
 		listItem.className = '';
+	}
+});
+
+ul.addEventListener('click', (e) => {
+	if (e.target.tagName === 'BUTTON') {
+		const button = e.target;
+		const li = button.parentNode;
+		const ul = li.parentNode;
+		if (button.textContent === 'remove') {
+			ul.removeChild(li);
+		} else if (button.textContent === 'edit') {
+			console.log('edit');
+		}
 	}
 });
